a4821404bb357b5423663dcd38d91bb08d7a5d15
Eliss Glasper <elissglasper@gmail.com>
2023-07-29 15:35:40 -0500

    cleaned up the project files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameManager : MonoBehaviour
+{
+    public int scoreToWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
